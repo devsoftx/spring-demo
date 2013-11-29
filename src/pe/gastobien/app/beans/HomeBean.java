163929ce815ed2d7026d6c7203deaff330eb31cb
Jose Fabricio Rojas <vitokofabri@gmail.com>
2013-11-28 22:41:04 -0500

    ok

--- src/pe/gastobien/app/beans/HomeBean.java
+++ src/pe/gastobien/app/beans/HomeBean.java
@@ -2,14 +2,31 @@ package pe.gastobien.app.beans;
 
 import java.io.Serializable;
 import java.util.*;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
 import pe.gastobien.app.domain.Category;
+import pe.gastobien.app.domain.CategoryDetail;
 import pe.gastobien.app.layer.service.interfaces.CategoryService;
 
 @Component
 public class HomeBean implements Serializable{
 	
+	private List<Category> categories;
+	
+	private List<CategoryDetail> details;
+	
+	private CategoryDetail detail; 
+	
+	public CategoryDetail getDetail() {
+		return detail;
+	}
+
+	public void setDetail(CategoryDetail detail) {
+		this.detail = detail;
+	}
+
 	@Autowired
 	private CategoryService service;
 	
@@ -18,9 +35,15 @@ public class HomeBean implements Serializable{
 	public void init() {
 		List<Category> categories = service.getCategories(1, 2013, 11);
 		setCategories(categories);
+	}	
+
+	public List<CategoryDetail> getDetails() {
+		return details;
 	}
-	
-	private List<Category> categories;
+
+	public void setDetails(List<CategoryDetail> details) {
+		this.details = details;
+	}
 
 	public List<Category> getCategories() {
 		return categories;
